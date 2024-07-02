606877af31446e05418223681b076f922c3d1f1f
Gura <tuananhtran180904@gmail.com>
2024-07-02 23:14:28 +0700

    second commit
    I acutally committed with my gitlab account 😭

--- A.cpp
+++ A.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 int main() {
 
-    cout << "Hello World";
+    cout << "Hello World" << endl;
+    cout << "This is the first cpp executable of this project" << endl;
+    cout << "Made by me!" << endl;
+    cout << "hehe" << endl;
 
     return 0;
 }
